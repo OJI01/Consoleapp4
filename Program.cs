65272aae0cd79ab91cceda4f1cb445b0c960573f
OJI01 <rahmatfauzi@students.amikom.ac.id>
2023-03-27 21:18:25 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MobilApp
+{
+    class Mobil
+    {
+        // Properti mobil
+        public string Warna { get; set; }
+        public int JumlahPintu { get; set; }
+        public string Merek { get; set; }
+        public string Model { get; set; }
+        public int TahunKeluaran { get; set; }
+
+        // Method mobil
+        public void HitungKecepatan(int jarak, int waktu)
+        {
+            double kecepatan = (double)jarak / waktu;
+            Console.WriteLine($"Kecepatan mobil adalah {kecepatan} km/jam\n");
+        }
+
+        public void Klakson1()
+        {
+            Console.WriteLine("bunyi klakson : Tin! Tin!");
+            Console.WriteLine("===================================\n");
+        }
+        public void Klakson2()
+        {
+            Console.WriteLine("bunyi klakson : beep! beep!");
+            Console.WriteLine("===================================\n");
+        }
+        public void Klakson3()
+        {
+            Console.WriteLine("bunyi klakson :Trenkkk! Trenkkk!");
+            Console.WriteLine("===================================\n");
+        }
+
+        public void TampilInfo()
+        {
+            Console.WriteLine($"Merek Mobil    : {Merek} \n");
+            Console.WriteLine($"Dengan model   : {Model} \n");
+            Console.WriteLine($"Variasi warna  : {Warna} \n");
+            Console.WriteLine($"Jumlah pintu   : {JumlahPintu} pintu\n");
+            Console.WriteLine($"Jeluaran tahun : {TahunKeluaran}\n");
+        }
+    }
+}
+
+    
---
+++ mobil.cs
@@ -0,0 +1,63 @@
+﻿using MobilApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class mobil
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Mobil mobil1 = new Mobil();
+                
+                // Set properti mobil
+                mobil1.Warna = "Putih";
+                mobil1.JumlahPintu = 2;
+                mobil1.Merek = "lamborgini";
+                mobil1.Model = "Veneno Roadster";
+                mobil1.TahunKeluaran = 2013;
+
+                // Panggil method mobil
+                mobil1.TampilInfo();
+                mobil1.Klakson1();
+                mobil1.HitungKecepatan(710, 2);
+
+                Mobil mobil2 = new Mobil();
+
+                // Set properti mobil
+                mobil2.Warna = "Hitam";
+                mobil2.JumlahPintu = 2;
+                mobil2.Merek = "Bugatti";
+                mobil2.Model = "Chiron";
+                mobil2.TahunKeluaran = 2018;
+
+                // Panggil method mobil
+                mobil2.TampilInfo();
+                mobil2.Klakson2();
+                mobil2.HitungKecepatan(620, 2);
+
+                Mobil mobil3 = new Mobil();
+
+                // Set properti mobil
+                mobil3.Warna = "Merah";
+                mobil3.JumlahPintu = 2; 
+                mobil3.Merek = "Ferari";
+                mobil3.Model = "J50";
+                mobil3.TahunKeluaran = 2016;
+
+                // Panggil method mobil
+                mobil3.TampilInfo();
+                mobil3.Klakson3();
+                mobil3.HitungKecepatan(1028, 2);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
